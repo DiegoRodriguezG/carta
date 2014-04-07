f414f9c59d065becf39fabeeb8b19d25b4fd948f
Jorge Vargas <jvargas@winnot.com>
2014-04-07 19:51:09 -0300

    se cambia ruta de deploy, ahora funca <3

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@gitlab.winnot.com:Dieguito/carta.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/admin/cocina'
+set :deploy_to, '/home/admin/carta'
 
 # Default value for :scm is :git
 # set :scm, :git
